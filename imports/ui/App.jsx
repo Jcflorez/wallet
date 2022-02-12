ceaf028d7fe4db1b339274f7ef94a046d8a3a25f
Jcflorez <johncarlos59@yahoo.es>
2022-02-12 14:29:43 -0500

    Anadiendo Tailwindcss Headder, Footer

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import {ContactForm} from './ContactForm';
 import { ContactList } from './ContactList';
+import { Footer } from './Footer';
+import { Header } from './Header';
 
-
-// Contact: name, email, imageURL
 export const App = () => (
-  <div>
-    <h1>Meteor Wallet</h1>
-    <ContactForm />
-    <ContactList/>
+<div>
+    <Header />
+    <div className="min-h-full">
+      <div className="max-w-4xl mx-auto p-2">
+        <ContactForm />
+        <ContactList />
+        <Footer />
+      </div>
+    </div>
   </div>
 );
---
+++ imports/ui/Header.jsx
@@ -0,0 +1,23 @@
+/* This example requires Tailwind CSS v2.0+ */
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <header className="bg-indigo-600">
+      <nav className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
+        <div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
+          <div className="flex items-center">
+            <a href="#">
+              <span className="">Meteor Wallet--</span>
+              <img
+                className="h-10 w-auto"
+                src="/images/tailwind-css.png"
+                alt=""
+              />
+            </a>
+          </div>
+        </div>
+      </nav>
+    </header>
+  )
+}
